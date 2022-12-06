72c944a56445e1bd678dd33c833a30510165a17f
hthcrwzy <119731650+hthcrwzy@users.noreply.github.com>
2022-12-06 20:46:22 +0900

    Initial Commit

---
+++ NiceToMeetYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiceToMeetYou
+//
+//  Created by Hejokyo 710 on 2022/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
